8029a24c04b9acf89384bde6f5922c6eb305917e
evertcarvalho <64963698+evertcarvalho@users.noreply.github.com>
2025-12-14 01:21:56 +0000

    adds `/app/v1/migrations` endpoit

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+//const { default: database } = require("infra/database");
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
